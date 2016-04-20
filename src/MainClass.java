febf35522ac28a6224d6a07923a22cd24323afe4
JustMo06 <mtemizka@avans.nl>
2016-04-20 15:50:45 +0200

    yo

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,12 +2,15 @@
  * @author Xavier van den Berg 2104964
  *
  */
-public class MainClass {
+public class MainClass
+{
 
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-	Game myGame = new Game();
+
+		System.out.println("Hello Universe!"); 	// persoon 2
+//		Game myGame = new Game();
 	}
 
 }
